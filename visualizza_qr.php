882cdfa409b3496ab13a23d9ab48ab4c84530b2f
mixedbyiado <info@mixedbyiado.com>
2025-06-09 19:09:49 +0200

    Update visualizza_qr.php

--- visualizza_qr.php
+++ visualizza_qr.php
@@ -35,6 +35,7 @@ if (isset($_GET['id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GastroQR - Visualizza Bollettino</title>
+    <link rel="stylesheet" href="style_viewqr.css">
 
 </head>
 <body>
